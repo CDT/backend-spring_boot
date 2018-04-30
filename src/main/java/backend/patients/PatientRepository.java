48e01ce18659955e0f0a22a04ea41980a46c7440
CDT <779888925@qq.com>
2018-04-30 21:53:16 +0800

    '.'

--- src/main/java/backend/patients/PatientRepository.java
+++ src/main/java/backend/patients/PatientRepository.java
@@ -1,6 +1,5 @@
 package backend.patients;
 
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,7 +10,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import backend.common.QueryResult;
-import backend.common.Utilities;
 import backend.patients.entity.PatientCard;
 
 @Repository
@@ -27,7 +25,7 @@ public class PatientRepository {
 	 */
 	public QueryResult getPatients(String ID, String name, boolean isFuzzy, int page, int per_page, String filter, String sort) {
 		String sqlTemplate = 
-				"select t.*, count(*) over()\r\n" + 
+				"select t.*\r\n" + 
 				"  from (select rownum rn,\r\n" + 
 				"               patient_id,\r\n" + 
 				"               name,\r\n" + 
@@ -35,23 +33,14 @@ public class PatientRepository {
 				"               date_of_birth,\r\n" + 
 				"               phone_number1,\r\n" + 
 				"               phone_number2,\r\n" + 
-				"               create_time\r\n" + 
+				"               create_time,\r\n" + 
+				"               count(*) over () as total_num_rows\r\n" +
 				"          from pca.pca_patient_info\r\n" + 
 				"         where (name like '%$filter%' or id_number like '%$filter%' or\r\n" + 
 				"               patient_id like '%$filter%' or\r\n" + 
-				"               phone_number1 like '%$filter%' or phone_number2 like '%%')) t\r\n" + 
-				" where rn BETWEEN ($pagenumber - 1) * $pagesize + 1 AND $pagenumber * $pagesize;
-				"SELECT res.* \r\n" + 
-						// "       CEIL(total_num_rows/$pagesize) total_num_pages\r\n" + 
-						"FROM   (SELECT o.*,\r\n" + 
-						"               rownum rn,\r\n" + 
-						"               COUNT(*) OVER () total_num_rows\r\n" + 
-						"        FROM PCA.PCA_PATIENT_INFO o\r\n" + 
-						"        WHERE (name like '%$filter%' or id_number like '%$filter%' " +
-						"		 or patient_id like '%$filter%' or phone_number1 like '%$filter%' " +
-						"		 or phone_number2 like '%$filter%')) res\r\n" + 
-						"WHERE  rn BETWEEN ($pagenumber - 1) * $pagesize + 1 AND $pagenumber * $pagesize";
-		
+				"               phone_number1 like '%$filter%' or phone_number2 like '%$filter%')) t\r\n" + 
+				" where rn BETWEEN ($pagenumber - 1) * $pagesize + 1 AND $pagenumber * $pagesize";
+				
 		String sql = sqlTemplate.replaceAll("\\$pagenumber", Integer.toString(page))
 				   				.replaceAll("\\$pagesize", Integer.toString(per_page))
 								.replaceAll("\\$filter", filter);
@@ -65,7 +54,7 @@ public class PatientRepository {
                 	PatientCard patient = new PatientCard(                
                 		rs.getString("patient_id"), 
                 		rs.getString("name"),
-                		rs.getString("gender"),
+                		rs.getString("sex_code"),
                 		rs.getDate("date_of_birth"),
                 		rs.getString("phone_number1"),
                 		rs.getString("phone_number2"),
